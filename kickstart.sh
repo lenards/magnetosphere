89692b3569767459d69cdd5cd345bbe7a6da983e
Andrew Lenards <andrew.lenards@gmail.com>
2016-02-26 10:10:04 -0700

    Add safe guard to where we kickstart; re-root to /vagrant

--- kickstart.sh
+++ kickstart.sh
@@ -1,16 +1,25 @@
-WORKSPACE="/vagrant"
+WORKSPACE="/vagrant/scratch"
 GIT_BRANCH="master"
 SERVER_NAME="localhost"
 SECRETS="$WORKSPACE/atmo-extras/clank_init/"
 ENV_FILE="$WORKSPACE/atmo-extras/clank_init/build_env/variables.yml@vagrant"
 
+# We only want to run this from within the context of a vagrant-box
+WORKING_DIR="$(pwd)"
+
+if [ "$WORKING_DIR" != "${WORKING_DIR/vagrant/}" ]; then
+    echo "Let's rock the automation...";
+else
+    echo "Please execute from within vagrant box...";
+    exit 5;
+fi
 
 # screw it - I'm writing functions...
 create_if_does_not_exist() {
     if [ ! -d "$1" ]; then
         echo "making directory & chown-ing... ";
         sudo mkdir -p "$1";
-        sudo chown vagrant:vagrant -R "$1";
+        sudo chown vagrant:www-data -R "$1";
     fi
 }
 
@@ -22,8 +31,24 @@ create_if_does_not_exist() {
 #0. Begin, eh
 cd "$WORKSPACE"
 
-create_if_does_not_exist "/opt/dev/atmosphere"
-create_if_does_not_exist "/opt/env/atmo"
+# 0.1 - crimes against automation...
+create_if_does_not_exist "/vagrant/scratch"
+create_if_does_not_exist "/vagrant/dev/atmosphere"
+create_if_does_not_exist "/vagrant/env/atmo"
+create_if_does_not_exist "/vagrant/dev/troposphere"
+create_if_does_not_exist "/vagrant/env/troposphere"
+create_if_does_not_exist "/vagrant/dev/atmosphere-ansible"
+
+# ensure that canonical pathing existing within vagrant-box
+ln -s /vagrant/dev/atmosphere /opt/dev/atmosphere
+ln -s /vagrant/dev/troposphere /opt/dev/troposphere
+ln -s /vagrant/dev/atmosphere-ansible /opt/dev/atmosphere-ansible
+
+# ensure that canonical `venv` are present within vagrant-box
+ln -s  /vagrant/env/atmo /opt/env/atmo
+ln -s  /vagrant/env/troposphere /opt/env/troposphere
+
+# move all of 0.1 into Clank at some __future___ date
 
 # 0.5
 #
@@ -47,10 +72,3 @@ pip install -r clank/ratchet_requirements.txt
 #3. Running ratchet
 cd "$WORKSPACE/clank"
 PYTHONUNBUFFERED=1 python ratchet.py --workspace $WORKSPACE --env_file $ENV_FILE
-
-#4. Chown to vagrant, activate and run!
-cd "$WORKSPACE"
-sudo chown -R vagrant:vagrant .
-cd "/opt/dev/atmosphere"
-. "/opt/env/atmo/bin/activate"
-
